3dbb1a8c1c871e4cc31c97cb15cfad79f3f99730
arifin <arifingdr@gmail.com>
2020-04-21 08:02:57 +0700

    fixed 21 april 20

--- test-workshop-master/src/main/java/com/workshop/sb/testworkshop/persistance/repository/DriverRepository.java
+++
@@ -1,7 +0,0 @@
-package com.workshop.sb.testworkshop.persistance.repository;
-
-import com.workshop.sb.testworkshop.persistance.model.Driver;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DriverRepository extends CrudRepository<Driver, Integer> {
-}
